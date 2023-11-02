c15f012636706177caeab111c5cad96ad645a3b4
Kasuntharu <rkasuntharu@gmail.com>
2023-11-02 17:44:08 +0000

    Edited

--- commit_service/main.py
+++ commit_service/main.py
@@ -2,18 +2,27 @@ import uvicorn
 from typing import Union
 
 from fastapi import FastAPI
-
+import requests
 app = FastAPI()
 
 
-@app.get("/")
+# @app.get("/{user}/{u}")
+# def read_root(user: str,u: int):
+#     print(u)
+#     url = f"https://api.github.com/users/{user}/repos"   
+#     response = requests.get(url)
+#     return response.json()
+
+@app.get("/commits")
 def read_root():
-    return {"Hello": "World"}
+    # print(u)
+    url = f"https://api.github.com/repos/kasuntharu/developer-iq/commits"   
+    response = requests.get(url)
+    return response.json()
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
 if __name__ == "_main_":
     uvicorn.run("_main_:app", host="0.0.0.0", port=8000, reload=True, workers=2)
